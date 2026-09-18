b797403eb0801afda8fb8cc0609542e32cfb5df0
we-BAK <hailemichaelkidist@gmail.com>
2026-09-18 16:22:33 +0300

    Beta version

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,15 +3,14 @@ require("dotenv").config();
 const app = require("./app");
 
 console.log("🟡 server.js started");
-
 console.log("🟡 About to load Telegram bot...");
 
 require("./bot/telegramBot");
 
 console.log("🟢 Telegram bot file loaded successfully");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 
-app.listen(PORT, () => {
-  console.log(`🚀 Backend running on port ${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`🚀 Backend running on 0.0.0.0:${PORT}`);
 });
