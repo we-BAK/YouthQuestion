c0e0da8f160476e52bae19c71eede5a41095ff2d
we-BAK <hailemichaelkidist@gmail.com>
2026-09-19 10:48:46 +0300

    prepared for deployment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
 import { AuthProvider } from "./context/AuthContext";
-
 export default function App() {
   return (
     <BrowserRouter>
